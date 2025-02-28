a7aedb0d1e0b3863c7cec3c9d5ba59049295e497
MwaleJ <joshua.mwale@tellistic.com>
2025-02-28 10:40:41 +0300

    updated the develop with yml file and deleted dataTable in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <Form/>
-      <h2 className="text-2xl font-bold mt-6"> Submitted Data</h2>
-      <DataTable/>
     </main>
   );
 }
